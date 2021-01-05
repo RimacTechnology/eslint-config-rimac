87756e4836184a40cc941b851976a5c2bb9e392b
vuki656 <off.vukovic@gmail.com>
2021-01-05 13:22:47 -0700

    ref(@typescript-eslint/comma-dangle): add dangling comma to imports

--- core.js
+++ core.js
@@ -26,7 +26,7 @@ module.exports = {
                 "arrays": "always-multiline",
                 "exports": "always",
                 "functions": "only-multiline",
-                "imports": "never",
+                "imports": "always-multiline",
                 "objects": "always-multiline"
             }
         ],
