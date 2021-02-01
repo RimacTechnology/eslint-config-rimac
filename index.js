dd78e5b60f3490967e299b299f2a93a358fa1558
domagojvukovic <domagoj.vukovic2@rimac-automobili.com>
2021-02-01 19:05:43 +0100

    feature(type-graphql): added type-graphql ruleset

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ module.exports = {
     core: require('./core'),
     react: require('./react'),
     jest: require('./jest'),
+    typeGraphql: require('./type-graphql'),
 }
---
+++ type-graphql.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "plugins": [ "type-graphql" ],
+    "rule": {
+        "invalid-decorated-type": "error",
+        "invalid-decorator-type": "error",
+        "invalid-nullable-input-type": "error",
+        "invalid-nullable-output-type": "error",
+        "missing-decorator-type": "error",
+        "wrong-decorator-signature": "error"
+    }
+}
