7ab9ecc6650b6972edcd14131bfc9ed8eeb6c06a
domagojvukovic <domagoj.vukovic2@rimac-automobili.com>
2021-07-20 10:02:00 +0200

    feat: add next ruleset

---
+++ next.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": ["next"],
+    "rules": {
+        "@next/next/google-font-display": "error",
+        "@next/next/google-font-preconnect": "error",
+        "@next/next/link-passhref": "error",
+        "@next/next/no-css-tags": "error",
+        "@next/next/no-document-import-in-page": "error",
+        "@next/next/no-head-import-in-document": "error",
+        "@next/next/no-html-link-for-pages": "error",
+        "@next/next/no-img-element": "error",
+        "@next/next/no-page-custom-font": "error",
+        "@next/next/no-sync-scripts": "error",
+        "@next/next/no-title-in-document-head": "error",
+        "@next/next/no-unwanted-polyfillio": "error"
+    }
+}
