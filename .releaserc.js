4a038c56752d2c4c51a455c3c5c999d167b735ba
mario.jerkovic <mario.jerkovic@rimac-automobili.com>
2022-01-14 10:10:15 +0100

    chore: add semantic release for version and release management

---
+++ .releaserc.js
@@ -0,0 +1,109 @@
+module.exports = {
+    branches: [
+        '+([0-9])?(.{+([0-9]),x}).x',
+        'master',
+        {
+            name: 'beta',
+            prerelease: true,
+        },
+        {
+            name: 'alpha',
+            prerelease: true,
+        },
+    ],
+    plugins: [
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                releaseRules: [
+                    {
+                        type: 'feat',
+                        release: 'minor',
+                    },
+                    {
+                        type: 'fix',
+                        release: 'patch',
+                    },
+                    {
+                        type: 'chore',
+                        release: 'minor',
+                    },
+                    {
+                        type: 'refactor',
+                        release: 'minor',
+                    },
+                    {
+                        type: 'perf',
+                        release: 'patch',
+                    },
+                ],
+            },
+        ],
+        [
+            '@semantic-release/release-notes-generator',
+            {
+                preset: 'conventionalcommits',
+                presetConfig: {
+                    commitUrlFormat: 'https://bitbucket.org/rimacautomobili/{{repository}}/commits/{{hash}}',
+                    compareUrlFormat: 'https://bitbucket.org/rimacautomobili/{{repository}}/branches/compare/{{currentTag}}%0D{{previousTag}}#diff',
+                    issueUrlFormat: 'https://rimac-automobili.atlassian.net/browse/{{id}}',
+                    userUrlFormat: 'https://bitbucket.org/rimacautomobili/users/{{user}}',
+                    types: [
+                        {
+                            type: 'feat',
+                            section: 'Features',
+                        },
+                        {
+                            type: 'fix',
+                            section: 'Bug Fixes',
+                        },
+                        {
+                            type: 'chore',
+                            section: 'Other',
+                        },
+                        {
+                            type: 'docs',
+                            section: 'Documentation',
+                        },
+                        {
+                            type: 'style',
+                            section: 'Styles',
+                        },
+                        {
+                            type: 'refactor',
+                            section: 'Code Refactoring',
+                        },
+                        {
+                            type: 'perf',
+                            section: 'Performance Improvements',
+                        },
+                        {
+                            type: 'test',
+                            section: 'Tests',
+                        },
+                        {
+                            type: 'ci',
+                            section: 'Continuous Integration',
+                        },
+                        {
+                            type: 'build',
+                            section: 'Build System',
+                        },
+                        {
+                            type: 'revert',
+                            section: 'Reverts',
+                        },
+                    ],
+                },
+            },
+        ],
+        '@semantic-release/changelog',
+        '@semantic-release/npm',
+        [
+            '@semantic-release/git',
+            {
+                message: 'release: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            },
+        ],
+    ],
+}
