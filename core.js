b886261f87552c8d8cb1bc15e60fbb63098d0389
domagoj.vukovic <off.vukovic@gmail.com>
2022-05-04 16:11:54 +0200

    feat(core): add @typescript-eslint/no-unnecessary-condition
    BREAKING CHANGE: New rule: @typescript-eslint/no-unnecessary-condition

--- core.js
+++ core.js
@@ -94,6 +94,7 @@ module.exports = {
         '@typescript-eslint/no-shadow': 'error',
         '@typescript-eslint/no-throw-literal': 'error',
         '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
+        '@typescript-eslint/no-unnecessary-condition': 'error',
         '@typescript-eslint/no-unnecessary-type-arguments': 'error',
         '@typescript-eslint/no-unnecessary-type-assertion': 'error',
         '@typescript-eslint/no-unnecessary-type-constraint': 'error',
